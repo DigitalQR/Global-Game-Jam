c99cbc2632c42429c0c9ab06c91ea7da9dcd4880
DigitalQR (Laptop) <sam_pot_97@hotmail.com>
2016-01-29 22:53:26 +0000

    Added Raw Player controllers

---
+++ Assets/Scripts/Player/GlobalPlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalPlayerManager{
+
+	//Holds the gamepad IDs for each player (-1 implies there is no player)
+	public static int[] playerGamepadID = {-1,-1,-1,-1};
+
+	//Returns true or false as to whether a player is pressing a certain number
+	public static bool GetButton(int playerNumber, string buttonName){
+		return Input.GetButton ("Gamepad " + playerGamepadID [playerNumber] + " " + buttonName);
+	}
+
+	//Returns the state of a specific button(axis) of a player
+	public static float GetAxis(int playerNumber, string buttonName){
+		return Input.GetAxis ("Gamepad " + playerGamepadID [playerNumber] + " " + buttonName);
+	}
+}
---
+++ Assets/Scripts/Player/RawPlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RawPlayerController : MonoBehaviour {
+	//Checks for input of a specific player
+
+	public int playerNumber = -1;
+
+	public bool isPressingUp(){
+		float value = GlobalPlayerManager.GetAxis(playerNumber, "Vertical");
+		return value == 1;
+	}
+
+	public bool isPressingDown(){
+		float value = GlobalPlayerManager.GetAxis(playerNumber, "Vertical");
+		return value == -1;
+	}
+
+	public bool isPressingRight(){
+		float value = GlobalPlayerManager.GetAxis(playerNumber, "Horizontal");
+		return value == 1;
+	}
+
+	public bool isPressingLeft(){
+		float value = GlobalPlayerManager.GetAxis(playerNumber, "Horizontal");
+		return value == -1;
+	}
+
+	public bool isPressingA(){
+		return GlobalPlayerManager.GetButton (playerNumber, "A");
+	}
+
+	public bool isPressingB(){
+		return GlobalPlayerManager.GetButton (playerNumber, "B");
+	}
+		
+	public bool isPressingStart(){
+		return GlobalPlayerManager.GetButton (playerNumber, "Start");
+	}
+
+	public bool isPressingSelect(){
+		return GlobalPlayerManager.GetButton (playerNumber, "Select");
+	}
+}
