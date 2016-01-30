fab28340935f127f03eeb7d34a37090ea3212ce6
DigitalQR (Laptop) <sam_pot_97@hotmail.com>
2016-01-30 09:19:43 +0000

    Basic board

---
+++ Assets/Scripts/Board/PieceController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceController : MonoBehaviour {
+
+	public int playerID;
+
+	public GameObject currentTile;
+	public GameObject lastTile;
+
+	int cooldown = 0;
+	int roll = 0;
+	float moveFactor = 0;
+
+	void Start(){
+		transform.position = currentTile.transform.position;
+	}
+
+	void FixedUpdate(){
+		if(cooldown != 0){
+			cooldown--;
+		}
+
+		if(GlobalPlayerManager.GetButton(playerID, "A") && cooldown == 0){
+			roll = Random.Range (1, 7);
+			moveFactor = 1;
+			Debug.Log (roll);
+
+			GameObject actualLastTile = currentTile;
+			currentTile= currentTile.GetComponent<TileController>().getTileThatIsnt(lastTile);
+			lastTile = actualLastTile;
+
+			cooldown = 7;
+		}
+
+		if(roll != 0){
+			if (moveFactor <= 0) {
+				moveFactor = 1;
+				roll--;
+
+				if(roll != 0){
+					GameObject actualLastTile = currentTile;
+					currentTile= currentTile.GetComponent<TileController>().getTileThatIsnt(lastTile);
+					lastTile = actualLastTile;
+				}
+
+			} else {
+				moveFactor -= 0.07f;
+				transform.position = lastTile.transform.position * (moveFactor) + currentTile.transform.position * (1f - moveFactor);
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/Board/TileController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour {
+
+	public GameObject[] nearbyTiles;
+
+	public GameObject getTileThatIsnt(GameObject tile){
+		for(int i = 0; i<nearbyTiles.Length; i++){
+			if (tile != nearbyTiles [i])
+				return (GameObject) nearbyTiles [i];
+		}
+		return tile;
+	}
+
+}
