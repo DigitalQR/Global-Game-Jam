47359f2a4343eaa7cb87828497a082146c7988f5
JonatanBiegalski <therealyonish@gmail.com>
2016-01-30 21:31:28 +0000

    Platforms, not finished mingame

---
+++ Assets/Scripts/Gamemodes/SideScrolling/SideScrollCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideScrollCamera : MonoBehaviour {
+    public bool isScrolling = false;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isScrolling = true && GameObject.FindGameObjectsWithTag("Player").Length > 1)
+        {
+            Transform.Translate(new Vector3(5 * Time.deltaTime, 0, 0));
+        }
+
+    }
+}
---
+++ Assets/Scripts/Gamemodes/SideScrolling/SideScrollController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideScrollController : MonoBehaviour {
+
+    bool firstLaunch = true;
+    // Use this for initialization
+    void OnEnable()
+    {
+        if (firstLaunch)
+        {
+            firstLaunch = false;
+            return;
+        }
+
+        Invoke("MakeScroll", 3);
+    }
+
+    void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	
+	}
+
+    void MakeScroll()
+    {
+        SideScrollCamera.isScrolling = true;
+    }
+}
