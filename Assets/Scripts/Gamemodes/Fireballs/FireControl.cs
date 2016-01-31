2dd7a52568dd5731109b9400a9cb1538a9e89697
JonatanBiegalski <therealyonish@gmail.com>
2016-01-31 09:14:46 +0000

    Fireballs minigame
    plus some random sprite tweaks

---
+++ Assets/Scripts/Gamemodes/Fireballs/FireControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireControl : MonoBehaviour {
+
+    public GameObject fireball;
+
+    bool firstLaunch = true;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        if (firstLaunch)
+        {
+            firstLaunch = false;
+            return;
+        }
+
+        Invoke("SpawnFireball", frequency-1);
+    }
+
+    float frequency = 3;
+
+    void SpawnFireball() {
+        Instantiate(fireball, new Vector3(Random.Range(11, 13), -2, 0), Quaternion.identity);
+
+        if (GameObject.FindGameObjectsWithTag("Player").Length > 1)
+        {
+            Invoke("SpawnFireball", frequency *= 0.95f);
+        }
+    }
+}
---
+++ Assets/Scripts/Gamemodes/Fireballs/Fireball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	// Use this for initialization
+	void Update() {
+        transform.Translate(new Vector3(-3 * Time.deltaTime, 0, 0));
+	}
+
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag.Equals("Player"))
+        {
+            collider.gameObject.GetComponent<RawPlayerController>().kill();
+        }
+    }
+}
